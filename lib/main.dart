0833ad5a02b8876dc7c9a494fbf89380d2ae5640
Blake Bottum <BlakeBottum@Blakes-Air.fios-router.home>
2018-05-20 04:33:28 -0500

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,35 @@ import 'package:flutter/material.dart';
 import 'dart:math';
 import 'dart:ui';
 import 'dart:io';
+import 'dart:convert';
 import 'dart:async';
 import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:http/http.dart' as http;
+
+Future<String> getData() async{
+  http.Response r = await http.get(
+    Uri.encodeFull("https://api.coinmarketcap.com/v2/listings")
+  );
+  data = json.decode(r.body);
+  //print(data.toString());
+  int runs = data["metadata"]["num_cryptocurrencies"];
+  itemCount = runs;
+  for(int i = 0; i<runs;i++){
+    fullList.add(new Crypto(data["data"][i]["website_slug"],Colors.black12,i,data["data"][i]["name"]));
+  }
+}
+
+int itemCount;
+
+Map<String, dynamic> data;
+
+String response;
 
 void main() {
+  getData();
   runApp(new MaterialApp(
     home: new HomePage()
   ));
@@ -52,7 +74,6 @@ List<Widget> favList = [
 ];
 
 List<Widget> fullList = [
-  new Crypto("Slug",Colors.black12,0)
 ];
 
 class CryptoList extends StatefulWidget{
@@ -62,8 +83,18 @@ class CryptoList extends StatefulWidget{
 }
 
 class CryproListState extends State<CryptoList>{
+
+  String search = "";
+
+  List<Widget> filteredList = new List<Widget>();
+
   @override
   Widget build(BuildContext context){
+    if(search==""){
+      if(filteredList.length==0){
+        filteredList.addAll(fullList);
+      }
+    }
     return new Scaffold(
       appBar: new AppBar(
         title: new TextField(
@@ -71,20 +102,31 @@ class CryproListState extends State<CryptoList>{
             autocorrect: false,
             decoration: new InputDecoration(
               hintText: "Search",
-              hintStyle: new TextStyle(color:Colors.white)
+              // ignore: conflicting_dart_import
+              hintStyle: new TextStyle(color:Colors.white),
+              prefixIcon: new Icon(Icons.search)
             ),
           style:new TextStyle(color:Colors.white),
+          onSubmitted: (s){
+              filteredList.clear();
+              search = s;
+              for(int i = 0; i<fullList.length;i++){
+                if((fullList[i] as Crypto).name.toUpperCase().contains(search.toUpperCase())){
+                  filteredList.add(fullList[i]);
+                }
+              }
+              setState(() {
+
+              });
+          }
         ),
         backgroundColor: Colors.black54,
       ),
       body: new Container(
         child: new Center(
-          child: new ListView(
-            children: [
-              new Column(
-                children: fullList
-              )
-            ]
+          child: new ListView.builder(
+            itemCount: itemCount,
+            itemBuilder: (BuildContext context,int index) => filteredList[index]
           )
         )
       )
@@ -104,7 +146,7 @@ class FavCrypto extends StatefulWidget{
 
   int index,friendIndex;
 
-  FavCrypto(this.slug,this.index,this.friendIndex);
+  FavCrypto(this.slug,this.index,this.friendIndex,this.name);
 
   @override
   FavCryptoState createState() => new FavCryptoState();
@@ -119,7 +161,6 @@ class FavCryptoState extends State<FavCrypto>{
     widget.oneHour = 10.0;
     widget.twentyFourHours = 9.0;
     widget.sevenDays = -8.0;
-    widget.name = "Bitcoin";
     widget.price = 15.00;
   }
 
@@ -144,7 +185,7 @@ class FavCryptoState extends State<FavCrypto>{
           child: new Row(
             children: <Widget>[
               // ignore: conflicting_dart_import
-              new Expanded(child: new Text("Bitcoin",style: new TextStyle(fontSize:25.0))),
+              new Expanded(child: new Text(widget.name,style: new TextStyle(fontSize:25.0))),
               new Expanded(child: new Text("\$10",style: new TextStyle(fontSize:25.0))),
               new Column(
                 crossAxisAlignment: CrossAxisAlignment.end,
@@ -179,7 +220,7 @@ class Crypto extends StatefulWidget{
 
   int index;
 
-  Crypto(this.slug,this.color,this.index);
+  Crypto(this.slug,this.color,this.index,this.name);
 
   @override
   CryptoState createState() => new CryptoState();
@@ -194,12 +235,15 @@ class CryptoState extends State<Crypto>{
     widget.oneHour = 10.0;
     widget.twentyFourHours = 9.0;
     widget.sevenDays = -8.0;
-    widget.name = "Bitcoin";
     widget.price = 15.00;
   }
 
   @override
   Widget build(BuildContext context){
+    widget.oneHour = 10.0;
+    widget.twentyFourHours = 9.0;
+    widget.sevenDays = -8.0;
+    widget.price = 15.00;
     return new Container(
         padding: EdgeInsets.only(top:10.0),
         child: new FlatButton(
@@ -225,7 +269,7 @@ class CryptoState extends State<Crypto>{
               setState((){widget.color = widget.color==Colors.black12?Colors.black26:Colors.black12;});
               Scaffold.of(context).showSnackBar(new SnackBar(content: new Text(widget.color==Colors.black26?"Added":"Removed"),duration: new Duration(seconds:1)));
               if(widget.color==Colors.black26){
-                favList.add(new FavCrypto(widget.slug, favList.length,widget.index));
+                favList.add(new FavCrypto(widget.slug, favList.length,widget.index,widget.name));
                 widget.favIndex = favList.length-1;
               }else{
                 favList.removeAt(widget.favIndex);
