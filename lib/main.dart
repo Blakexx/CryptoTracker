298a7fb0245fdc2008701012f7114767824dcdd3
Blake Bottum <blakeplatypus@gmail.com>
2018-06-10 22:38:02 -0500

    Update to 1.0.4

--- lib/main.dart
+++ lib/main.dart
@@ -587,7 +587,7 @@ class HomePageState extends State<HomePage>{
                                         children: <Widget>[
                                           new Text("\n"),
                                           Image.asset("icon/platypus2.png",height:150.0*(MediaQuery.of(context).size.width<=MediaQuery.of(context).size.height?MediaQuery.of(context).size.width:MediaQuery.of(context).size.height)/375.0,width:150.0*(MediaQuery.of(context).size.width<=MediaQuery.of(context).size.height?MediaQuery.of(context).size.width:MediaQuery.of(context).size.height)/375.0),
-                                          new Text("\nPlatypus Crypto V1.0.3"),
+                                          new Text("\nPlatypus Crypto V1.0.4"),
                                           new Text("©2018 Blake Bottum and Caleb Jiang",style: new TextStyle(fontWeight:FontWeight.bold))
                                         ]
                                     )
